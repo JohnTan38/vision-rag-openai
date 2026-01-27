5b424f2c6da02f4f33528c03450e632b782c81d3
John <appy1828@gmail.com>
2026-01-27 21:18:06 +0800

    Add files via upload

--- types/pdfjs-dist-legacy.d.ts
+++ types/pdfjs-dist-legacy.d.ts
@@ -1,1 +1,5 @@
-declare module 'pdfjs-dist/legacy/build/pdf';
+declare module 'pdfjs-dist/legacy/build/pdf' {
+  export const getDocument: any;
+  export const GlobalWorkerOptions: any;
+  export const version: string;
+}
